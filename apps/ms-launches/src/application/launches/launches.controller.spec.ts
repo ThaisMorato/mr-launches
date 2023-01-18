29e5e30393b5abb309dfd6446beb9cbd2f25d5ce
Thais Morato <thaisavilam@gmail.com>
2023-01-17 22:22:21 -0300

    #TASK-BACKEND-4

--- apps/ms-launches/src/application/launches/launches.controller.spec.ts
+++ apps/ms-launches/src/application/launches/launches.controller.spec.ts
@@ -66,4 +66,34 @@ test('should try to add a new launch and return a validation error', async () =>
 
 /*
     #TASK-BACKEND-4 (TESTS/JEST)- WRITE A TEST TO VALIDATE ALL THE FLOW IMPLEMENTED.
-  */
+*/
+test('should fetch launches in order', async () => {
+    const launchesController = await new LaunchesController(
+        router,
+        new LaunchesService(launchesRepositoryMock)
+    )
+
+    const baseLaunchObj = {
+        rocket: { id: 1, name: "RocketName" },
+        rocketId: 1,
+        success: true,
+        launchCode: '123456',
+    };
+    const launches = new Promise<LaunchesDTO[]>((resolve) => resolve(
+        [
+            { ...baseLaunchObj, id: 1, date: new Date("2021-10-10") },
+            { ...baseLaunchObj, id: 2, date: new Date("2022-10-10") },
+            { ...baseLaunchObj, id: 3, date: new Date("2023-10-10") },
+        ]
+    ));
+
+    jest
+    .spyOn(launchesRepositoryMock, 'findByName')
+    .mockReturnValueOnce(launches)
+
+    const launchesFetched = await launchesController.getLauchesByRocket('')
+    expect(launchesFetched.length).toBe(3);
+    expect(launchesFetched[0].date.getFullYear()).toBe(2023)
+    expect(launchesFetched[1].date.getFullYear()).toBe(2022)
+    expect(launchesFetched[2].date.getFullYear()).toBe(2021)
+})
